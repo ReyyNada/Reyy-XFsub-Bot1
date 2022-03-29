d64f60a938f838c31397b8672335be52fca594e6
ichigoxynn <94505216+ReyyNada@users.noreply.github.com>
2022-03-29 22:16:54 +0700

    Update helper_func.py

--- helper_func.py
+++ helper_func.py
@@ -4,22 +4,18 @@ import base64
 import re
 import asyncio
 from pyrogram import filters
-from config import FORCE_SUB_CHANNEL1, FORCE_SUB_CHANNEL2, ADMINS
+from config import FORCE_SUB_CHANNEL1, ADMINS
 from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant
 from pyrogram.errors import FloodWait
 
 async def is_subscribed(filter, client, update):
     if not FORCE_SUB_CHANNEL1:
         return True
-    if not FORCE_SUB_CHANNEL2:
-        return True
     user_id = update.from_user.id
     if user_id in ADMINS:
         return True
     try:
         member = await client.get_chat_member(chat_id=FORCE_SUB_CHANNEL1, user_id=user_id)
-        member = await client.get_chat_member(chat_id=FORCE_SUB_CHANNEL2, user_id=user_id)
-    except UserNotParticipant:
         return False
 
     return member.status in ["creator", "administrator", "member"]

6b1fecc023e674f11ce84be83273dc2d25fcf35f
shiroxynn <94505216+ReyyNada@users.noreply.github.com>
2022-04-14 05:09:57 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -46,7 +46,7 @@ class Bot(Client):
             sys.exit()
 
         self.set_parse_mode("html")
-        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by ichigo xynn\nhttps://t.me/IchigoEX")
+        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by shiro xynn\nhttps://t.me/shiroxynn")
         self.username = usr_bot_me.username
 
     async def stop(self, *args):
